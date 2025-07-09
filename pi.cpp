c1429e13a3b6e3ba0090739e5cbacafef3f38679
ReabetsweMeraba <reabetswemeraba@gmail.com>
2025-07-09 12:47:46 +0200

    Create pi.cpp

---
+++ pi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+void PiSqrt(){
+    double pi=3.141592654;
+    cout << "sqrtpi = " << sqrt(pi) << endl;
+    
+}
+
+int main()
+{
+    PiSqrt();
+
+    return 0;
+}
